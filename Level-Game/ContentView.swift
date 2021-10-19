b95c97aabbc55a6a453f85bfeb890059b3753dc1
Mohmed Anis Agwan <anishagwan123@gmail.com>
2021-10-19 12:26:49 +0530

    Initialised project

---
+++ Level-Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Level-Game
+//
+//  Created by Anis on 19/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Level-Game/Level_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Level_GameApp.swift
+//  Level-Game
+//
+//  Created by Anis on 19/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Level_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
